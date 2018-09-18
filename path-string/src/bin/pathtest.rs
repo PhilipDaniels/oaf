19f5244e170cb97f918ea1d727521aa1ce1e626f
Philip Daniels <Philip.Daniels1971@gmail.com>
2018-09-18 20:13:19 +0100

    Change to use WalkDir rather than find.

--- path-string/src/bin/pathtest.rs
+++ path-string/src/bin/pathtest.rs
@@ -1,42 +1,39 @@
 extern crate path_string;
 extern crate separator;
+extern crate walkdir;
 
-use std::io;
-use std::io::prelude::*;
-use std::ffi::OsString;
-use std::os::unix::ffi::OsStringExt;
 use std::path::PathBuf;
 use std::borrow::Cow;
 use separator::Separatable;
+use walkdir::WalkDir;
 
-struct PathRequiringEncoding {
+struct EncodedPath {
     original_path: PathBuf,
-    encoded_form: String,
+    encoded_path: String,
 }
 
-// Recommended usage     : find DIR -print0 | target/release/pathtest
-// Scan your whole drive : sudo find / -print0 | target/release/pathtest
-
 fn main() {
-    println!("Counting paths according to base64 encoding needs.");
+    let start_dir = if std::env::args().len() > 1 {
+        PathBuf::from(std::env::args().nth(1).unwrap())
+    } else {
+        std::env::current_dir().expect("Could not determine current directory.")
+    };
 
-    let stdin = io::stdin();
+    println!("Counting paths below {} according to encoding needs.", start_dir.display());
+
     let mut total_paths = 0;
     let mut num_borrowed = 0;
     let mut num_encoded = 0;
-    let mut paths_needing_encoding = Vec::new();
+    let mut encoded_paths = Vec::new();
 
-    for p in stdin.lock().split(0).take(2_000_000) {
-        let path_as_bytes = p.unwrap();
-        //println!("Reading path_as_bytes = {:?}", path_as_bytes);
-        let os = OsString::from_vec(path_as_bytes);
-        //println!("Reading os = {:?}", os);
-        let pb = PathBuf::from(os);
-        let coded = path_string::path_to_path_string(&pb);
-
+    // Recursively walk from start_dir, ignoring any errors (which occur for
+    // such things as 'permission denied'.
+    for entry in WalkDir::new(start_dir).into_iter().filter_map(|e| e.ok()) {
         total_paths += 1;
+        let pb = entry.into_path();
+        let possibly_encoded_path = path_string::path_to_path_string(&pb);
 
-        match coded {
+        match possibly_encoded_path {
             Cow::Borrowed(_s) => num_borrowed += 1,
             Cow::Owned(encoded_path) => {
                 num_encoded += 1;
@@ -44,20 +41,21 @@ fn main() {
                 println!("  Found a path requiring encoding: {:?}", pb);
                 println!("  Encoded form is                : {:?}", encoded_path);
                 println!("  Round tripped back to path is  : {:?}", round_tripped);
+                // Assert here, so the program will stop with the offender as the last thing
+                // printed.
                 assert_eq!(pb, round_tripped);
 
                 // Accumulate these for printing them all at the end.
-                let pre = PathRequiringEncoding { original_path: pb.clone(), encoded_form: encoded_path };
-                paths_needing_encoding.push(pre);
+                let pre = EncodedPath { original_path: pb.clone(), encoded_path };
+                encoded_paths.push(pre);
             }
         }
 
         if total_paths % 1000 == 0 {
             print_msg(num_borrowed, num_encoded);
-
         }
     }
-    
+
     print_msg(num_borrowed, num_encoded);
     println!("Counting complete.");
 
@@ -68,15 +66,14 @@ fn main() {
                  num_encoded, total_paths.separated_string());
         println!("This represents {}% of the total path count.\n", percentage);
 
-        for pre in paths_needing_encoding {
-            println!("Original path: {:?}", pre.original_path);
-            println!("Encoded form : {:?}", pre.encoded_form);
+        for ep in encoded_paths {
+            println!("Original path: {:?}", ep.original_path);
+            println!("Encoded form : {:?}", ep.encoded_path);
         }
     }
 }
 
 fn print_msg(num_borrowed: usize, num_encoded: usize) {
-    println!("num_borrowed = {}, num_encoded = {}",
-             num_borrowed, num_encoded);
+    println!("num_borrowed = {}, num_encoded = {}", num_borrowed, num_encoded);
 }
 
